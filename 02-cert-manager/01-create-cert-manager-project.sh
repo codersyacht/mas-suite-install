1b044b913ca5ddfcb5584cc901283e618f48311a
codersyacht <codersyacht@gmail.com>
2026-09-16 01:54:57 +0000

    mas-suite-install

---
+++ 02-cert-manager/01-create-cert-manager-project.sh
@@ -0,0 +1,2 @@
+oc new-project cert-manager
+echo "create project cert-manager executed successfully"
--- 02-common-services/01-create-ibm-common-services-project.sh
+++
@@ -1,2 +0,0 @@
-oc new-project ibm-common-services
-echo "create project ibm-oms-services executed successfully"
